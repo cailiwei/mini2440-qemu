5bc60fc99cec77eae827893141d36f6631f0bafe
Levycai <levycai516@gmail.com>
2013-11-25 09:46:15 +0800

    Modified the configure error

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build qemu for mini2440
 # make clean
-sudo apt-get install libsdl-dev
+sudo apt-get install zlib1g libglib2.0-dev libsdl-dev
 ./configure --target-list=arm-softmmu --prefix=$HOME/bin
 make | tee build.info
 echo "================================================"
